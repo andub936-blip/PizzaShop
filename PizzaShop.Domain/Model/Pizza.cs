6378507dd70a33d462c8df2384c5a64e2f905b89
User <User@LAPTOP-4H5VF20M>
2026-08-12 10:07:07 +0500

    Добавьте файлы проекта.

---
+++ PizzaShop.Domain/Model/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaShop.Domain.Model
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PizzaShop/Controllers/PizzaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaShop.Domain.Model;
+
+namespace PizzaShop.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private List<Pizza> _pizzas = new()
+        {
+            new Pizza { Id = 1, Name = "Margherita", Price = 15.12m, Description = "Pizza with tomatoes, mozzarella, basil, extra virgin olive oil" },
+            new Pizza { Id = 2, Name = "Pepperoni", Price = 12.99m, Description = "Pizza with pepperoni and mozzarella" },
+            new Pizza { Id = 3, Name = "Four Cheese", Price = 15.12m, Description = "Pizza with Mozzarella, Gorgonzola, Fontina, Parmesan" }
+        };
+        [HttpGet("pizzas")]
+        public IActionResult Index()
+        {
+            ViewData["PizzaList"] = _pizzas;
+
+            return View();
+        }
+
+        [HttpGet("pizzas/{id}")]
+        public IActionResult Details(int id)
+        {
+            var pizza = _pizzas.FirstOrDefault(p => p.Id == id);
+
+            if (pizza is null) return NotFound();
+
+            ViewData["SelectedPizza"] = pizza;
+
+            return View();
+        }
+
+        [HttpGet("pizzas/status")]
+        public IActionResult Status()
+        {
+            return Ok("Status OK");
+        }
+    }
+}
