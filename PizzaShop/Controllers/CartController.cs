82ff712cce054e3406a0455b1b8d156adc343f7a
Anton <await>
2026-08-17 10:50:34 +0500

    add pizza to cart

---
+++ PizzaShop/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaShop.ViewModels.CartVM;
+
+namespace PizzaShop.Controllers
+{
+    public class CartController : Controller
+    {
+        [HttpPost("cart/add")]
+        public IActionResult Add([FromForm] AddToCartViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            return Ok(viewModel);
+        }
+    }
+}
